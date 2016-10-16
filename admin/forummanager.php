f54f2dedec6a943ce8fd777bcdc879cc793ca0fe
Mindless <bigjoos1@gmail.com>
2016-10-16 14:27:29 +0000

    Header redirect fix by yoooov;

--- admin/forummanager.php
+++ admin/forummanager.php
@@ -73,9 +73,9 @@ case 'process_edit' :
 		break;
 	}
 	if(sql_query($res)) {
-		
+		header('Refresh:2; url='.$this_url);
 		stderr('Success',$msg);
-                header('Refresh:2; url='.$this_url);
+                
 	} else
 		stderr('Err','Something happened! Mysql Error '.((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
 break;
