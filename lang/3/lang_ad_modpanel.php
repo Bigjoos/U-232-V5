e9a378f20ef21f4ce69af4e3b06d1c5cd668c729
Mindless <bigjoos1@gmail.com>
2016-10-18 00:46:16 +0000

    New lang files;

---
+++ lang/3/lang_ad_modpanel.php
@@ -0,0 +1,15 @@
+<?php
+$lang = array(
+	//MOD_PANEL.PHP TERMS == NEW FILE
+	'mod_mp_opt'  => "Staff options",
+	'mod_mp_stk'  => "Sticky:",
+	'mod_mp_lck'  => "Locked:",
+	'mod_mp_name'  => "Topic name:",
+	'mod_mp_move'  => "Move topic:",
+	'mod_mp_del'  => "Delete topic:",
+	'mod_mp_yes'  => "Yes",
+	'mod_mp_no'  => "No",
+	'mod_mp_note'  => "<b>Note:</b> Any changes made to the topic won't take effect if you select <b>yes</b>",
+	'mod_mp_uptop'  => "Update Topic"
+);
+?>
