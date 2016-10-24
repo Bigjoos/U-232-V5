140293df8009393f1b985b00f2c0ce05fe8a0daa
Mindless <bigjoos1@gmail.com>
2016-10-24 10:11:21 +0000

    New lang file by Yoooov;

---
+++ lang/1/lang_ad_artefact.php
@@ -0,0 +1,40 @@
+<?php
+$lang = array(
+	//GENRE addition terms by yoooov
+	'upload_add_genre'  => "Genre",
+	'upload_add_movie'  => "Movie",
+	'upload_add_music'  => "Music",
+	'upload_add_game'  => "Game",
+	'upload_add_apps'  => "Apps",
+	'upload_add_none'  => "None",
+	//
+	'movie_mv1'  => "Action",
+	'movie_mv2'  => "Comedy",
+	'movie_mv3'  => "Thriller",
+	'movie_mv4'  => "Adventure",
+	'movie_mv5'  => "Family",
+	'movie_mv6'  => "Crime",
+	'movie_mv7'  => "Sci-fi",
+	//
+	'music_m1'   => "Hip Hop",
+	'music_m2'   => "Rock",
+	'music_m3'   => "Pop",
+	'music_m4'   => "Jazz",
+	'music_m5'   => "Electronic",
+	'music_m6'   => "Autre",
+	//
+	'game_g1'    => "Crime",
+	'game_g2'    => "Drama",
+	'game_g3'    => "Thriller",
+	'game_g4'    => "Sci-fi",
+	'game_g5'    => "Autre",
+	//
+	'app_mv1'    => "Burning",
+	'app_mv2'    => "Encoding",
+	'app_mv3'    => "Anti-Virus",
+	'app_mv4'    => "Office",
+	'app_mv5'    => "Os",
+	'app_mv6'    => "Misc",
+	'app_mv7'    => "Image"
+);
+?>
