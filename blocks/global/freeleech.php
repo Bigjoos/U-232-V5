178fdd51c421eb904ef9187f67e563167b756a6c
whocares <whocares@open-scene.net>
2016-11-21 14:26:48 -0500

    Update freeleech.php

--- blocks/global/freeleech.php
+++ blocks/global/freeleech.php
@@ -18,7 +18,7 @@
  */
 //== Free torrents begin
 if ($CURUSER) {
-    if (isset($free) && (count($free) >= 1)) {
+    if (isset($free) && is_array($free) && (count($free) >= 1)) {
         foreach ($free as $fl) {
             switch ($fl['modifier']) {
             case 1:
