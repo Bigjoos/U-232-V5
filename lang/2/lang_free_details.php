25c1b93fb8ab65595b4fd4bde679235b08be5b76
iseeyoucopy <ivan.catalin.aurel@gmail.com>
2017-02-12 09:45:46 +0000

    Create lang_free_details.php
    updated missing information

---
+++ lang/2/lang_free_details.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ |--------------------------------------------------------------------------|
+ |   https://github.com/Bigjoos/                                            |
+ |--------------------------------------------------------------------------|
+ |   Licence Info: WTFPL                                                    |
+ |--------------------------------------------------------------------------|
+ |   Copyright (C) 2010 U-232 V5                                            |
+ |--------------------------------------------------------------------------|
+ |   A bittorrent tracker source based on TBDev.net/tbsource/bytemonsoon.   |
+ |--------------------------------------------------------------------------|
+ |   Project Leaders: Mindless, Autotron, whocares, Swizzles.               |
+ |--------------------------------------------------------------------------|
+  _   _   _   _   _     _   _   _   _   _   _     _   _   _   _
+ / \ / \ / \ / \ / \   / \ / \ / \ / \ / \ / \   / \ / \ / \ / \
+( U | - | 2 | 3 | 2 )-( S | o | u | r | c | e )-( C | o | d | e )
+ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/ \_/ \_/   \_/ \_/ \_/ \_/
+ */
+$lang = array(
+	'mod_free_down' => "Free download",
+	'mod_free_tsilver' => "Silver Torrent",
+	'mod_free_alltfree' => "All Torrents Free",
+	'mod_free_dupload' => "All Double Upload",
+	'mod_free_tfree_dupload' => "All Torrents Free and Double Upload",
+	'mod_free_all_tsilver' => "All Torrents Silver",
+	'mod_free_fstatus' => "Free Status",
+	'mod_free_torfree' => "Torrent FREE",
+	'mod_free_togo' => " to go",
+	'mod_free_unlimited' => "Unlimited",
+	'mod_free_torsilver' => "Torrent SILVER",
+	'mod_free_expires' => " Expires: ",
+	'mod_free_pstatus' => "Personal FREE Status "
+);
+?>
