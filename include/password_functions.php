17779f2415aeeb814225c6ecfa9f81dab779dd1a
iseeyoucopy <iseeyoucopy@yahoo.com>
2020-04-13 22:31:22 +0100

    update password_functions.php
    Updating php password function from md5() to password_hash()
    Removed function make_password()  from file

--- include/password_functions.php
+++ include/password_functions.php
@@ -31,26 +31,9 @@ function mksecret($len = 5)
 function make_passhash_login_key($len = 60)
 {
     $pass = mksecret($len);
-    return md5($pass);
+    return password_hash($pass, PASSWORD_DEFAULT);
 }
-function make_passhash($salt, $md5_once_password)
-{
-    return md5(md5($salt) . $md5_once_password);
-}
-function make_password()
-{
-    $pass = "";
-    $unique_id = uniqid(mt_rand() , TRUE);
-    $prefix = mksecret();
-    $unique_id.= md5($prefix);
-    usleep(mt_rand(15000, 1000000));
-    mt_srand((double)microtime() * 1000000);
-    $new_uniqueid = uniqid(mt_rand() , TRUE);
-    $final_rand = md5($unique_id . $new_uniqueid);
-    mt_srand();
-    for ($i = 0; $i < 15; $i++) {
-        $pass.= $final_rand{mt_rand(0, 31) };
-    }
-    return $pass;
+function make_passhash($password) {
+    return password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
 }
 ?>
